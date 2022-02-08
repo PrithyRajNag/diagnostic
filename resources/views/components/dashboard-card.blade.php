f4bfddcb6978bcc5f5b8c52eedf6ed003d254f5e
debjitpaul-Inno <pauldebjit84@gmail.com>
2022-02-08 21:40:47 +0600

    composer updated

--- resources/views/components/dashboard-card.blade.php
+++ resources/views/components/dashboard-card.blade.php
@@ -1,16 +1,18 @@
 <div class="col-12 col-md-3">
-{{--    <div class="card card-statistic">--}}
-{{--        <div class="card-body p-0">--}}
-{{--            <div class="d-flex flex-column">--}}
-{{--                <div class='px-3 py-3 d-flex justify-content-between'>--}}
-{{--                    <h3 class='card-title'>{{$title}}</h3>--}}
-{{--                    <div class="card-right d-flex align-items-center">--}}
-{{--                        <p>{{$value}}</p>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+   {{-- <div class="card card-statistic"> --}}
+       {{-- <div class="card-body p-0"> --}}
+           {{-- <div class="d-flex flex-column"> --}}
+               {{-- <div class='px-3 py-3 d-flex justify-content-between'> --}}
+                   {{-- <h3 class='card-title'>{{$title}}</h3> --}}
+                   {{-- <div class="card-right d-flex align-items-center"> --}}
+                       {{-- <p>{{$value}}</p> --}}
+                   {{-- </div> --}}
+               {{-- </div> --}}
+           {{-- </div> --}}
+       {{-- </div> --}}
+   {{-- </div> --}}
+
+{{-- flip card --}}
     <div class="card {{$cssclass}} flip-card">
         <div class="flip-card-inner p-0 card-body">
             <div class="flip-card-front d-flex flex-column">
